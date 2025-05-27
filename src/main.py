f0aff4c644540db8b3540026e2be2d3eb4e7ed5c
andreslauria <andreslauria@gmail.com>
2025-05-27 01:57:05 +0000

    main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
-import textnode
+import sys
+from pathlib import Path
+import shutil
+
+from block_markdown import markdown_to_html_node
+from htmlnode import HTMLNode
+
+def copy_directory_recursive(src, dst):
+   if not dst.exists():
+      dst.mkdir()
+   
+   for file in src.iterdir():
+      new_f = dst / file.name
+      if file.is_dir():
+            copy_directory_recursive(f, new_f)
+      else:
+            print(f"Copying {f} to {new_f}")
+            shutil.copy(file, new_f)
 
 
 def main():
-   print(textnode.TextNode("This is some anchor text", textnode.TextType.LINKS, "https://www.ande.dev" ))
-    
-
+   pass
+   
+   
 main()
